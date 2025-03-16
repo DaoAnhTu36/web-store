5dd9468d90411a2babf3562822549f8ae72cefed
TuTran <trailangnd96@gmail.com>
2025-03-16 15:13:17 +0700

    [UPDATE] add vikew supplier transaction

---
+++ app/Views/admin/supplier_view/index_view.php
@@ -0,0 +1,126 @@
+<?= $this->extend('admin/Layouts/main_view.php'); ?>
+<?= $this->section('content'); ?>
+<?php if (session()->getFlashdata('errors')): ?>
+    <div class="alert alert-danger">
+        <?= implode('<br>', session()->getFlashdata('errors')); ?>
+    </div>
+<?php endif; ?>
+
+<?php if (session()->getFlashdata('success')): ?>
+    <div class="alert alert-success">
+        <?= session()->getFlashdata('success'); ?>
+    </div>
+<?php endif; ?>
+
+<!-- MAIN CONTENT -->
+<div id="content">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <h1 class="page-title txt-color-blueDark">
+                <i class="fa fa-list-alt fa-fw "></i>
+                <?= $title ?>
+                <!-- <span>>
+                    Normal Tables
+                </span> -->
+            </h1>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="well">
+                <div class="btn-group">
+                    <a href="<?= site_url('admin/supplier/create'); ?>" class="btn btn-default"><i class="fa fa-plus" aria-hidden="true"></i> Thêm mới</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- widget grid -->
+    <section id="widget-grid" class="">
+
+        <!-- row -->
+        <div class="row">
+
+
+            <!-- Widget ID (each widget will need unique ID)-->
+            <div class="jarviswidget jarviswidget-color-darken" id="wid-id-0" data-widget-editbutton="false">
+                <!-- widget options:
+								usage: <div class="jarviswidget" id="wid-id-0" data-widget-editbutton="false">
+				
+								data-widget-colorbutton="false"
+								data-widget-editbutton="false"
+								data-widget-togglebutton="false"
+								data-widget-deletebutton="false"
+								data-widget-fullscreenbutton="false"
+								data-widget-custombutton="false"
+								data-widget-collapsed="true"
+								data-widget-sortable="false"
+				
+								-->
+
+                <!-- widget div-->
+                <div>
+
+                    <!-- widget edit box -->
+                    <div class="jarviswidget-editbox">
+                        <!-- This area used as dropdown edit box -->
+
+                    </div>
+                    <!-- end widget edit box -->
+
+                    <!-- widget content -->
+                    <div class="widget-body no-padding">
+
+                        <table id="dt_basic" class="table table-striped table-bordered table-hover" width="100%">
+
+                            <thead>
+                                <tr>
+                                    <th>STT</th>
+                                    <th>ID</th>
+                                    <th>Tên nhà cung cấp</th>
+                                    <th>Email</th>
+                                    <th>Số điện thoại</th>
+                                    <th>Địa chỉ</th>
+                                    <th>Logo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $idx = 1;
+                                foreach ($data as $item) { ?>
+                                    <tr>
+                                        <td><?= $idx ?></td>
+                                        <td><?= $item['id'] ?></td>
+                                        <td><?= $item['name'] ?></td>
+                                        <td><?= $item['email'] ?></td>
+                                        <td><?= $item['phone'] ?></td>
+                                        <td><?= $item['address'] ?></td>
+                                        <td>
+                                            <?php
+                                            $images = trim(explode(', ', $item['images'])[0]);
+                                            ?>
+                                            <img src="<?= base_url($images) ?>" alt="" width="50">
+                                            <?php  ?>
+                                        </td>
+                                    </tr>
+                                <?php $idx++;
+                                } ?>
+                            </tbody>
+                        </table>
+
+                    </div>
+                    <!-- end widget content -->
+
+                </div>
+                <!-- end widget div -->
+
+            </div>
+            <!-- end widget -->
+
+        </div>
+
+        <!-- end row -->
+
+    </section>
+    <!-- end widget grid -->
+</div>
+<!-- END MAIN CONTENT -->
+
+<?= $this->endSection(); ?>
